2ad7281f704cabd3a87aaf8d51d0fb177c0755ed
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-04 00:44:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,9 @@ const delay = document.getElementById("delay");
 const btn = document.getElementById("btn");
 const output = document.getElementById("output");
 
-//your code here
 async function showMessage() {
   const message = text.value;
   const delayVal = delay.value;
-	 if (!message || isNaN(delayVal) || delayVal <= 0) {
-    output.innerText = "Invalid input";
-    return;
-  }
-	
   await new Promise((resolve) => setTimeout(resolve, delayVal));
   output.innerText = message;
 }
